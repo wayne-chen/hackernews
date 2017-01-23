3e278c591ae9e34a4d6f53363fece2366d1efccf
Wayne Chen <chen.wayne@outlook.com>
2017-01-23 15:58:56 -0800

    adding tutorial pdf and second stage. second stage of the application involves creating <div>s and creating a list.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+    {
+        title: 'Playing with React', 
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = "Welcome to React!"
     return (
         <div className="App">
-          <h2>{helloWorld}</h2>
+            {
+                list.map(function(item){
+                    return (
+                        <div key={item.objectID}>
+                            <div><a href={item.url}>{item.title}</a></div>
+                            <div>Authors: {item.author}</div>
+                            <div>Comments({item.num_comments})</div>
+                            <div>{item.points} points</div>
+                        </div>
+                    );
+                })}
         </div>
     );
   }
